e19a216299e25e97e42fdaf523d255e00ed6da0b
RozierT <135091431+RozierT@users.noreply.github.com>
2023-11-08 19:36:48 -0800

    added footer with links and centered

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Nav />
       <main className="p-3 mb-2 bg-info-subtle text-emphasis-secondary" style={{minHeight: '100vh'}}>
         <Outlet />
+        <Footer />
       </main>
     </>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,36 @@
 import { Link } from 'react-router-dom';
+import Github from '../assets/images/github.png';
+import LinkedIn from '../assets/images/linkedin.png';
 
 function Footer() {
 
   return (
-    <div>
-    <ul>
-      <li> GitHub: <Link to="https://github.com/RozierT"> GitHub </Link>
-        </li>
-      <li>LinkedIn: <Link to="https://www.linkedin.com/in/taylor-rozier-a76247105/"> LinkedIn</Link>
-        </li>
-      <li> Email: <a href="mailto:Roziertaylor@gmail.com">roziertaylor@gmail.com</a>
-        </li>
-    </ul>
+    <div className="d-flex flex-column vh-100 justify-content-end align-items-center">
+      <div className="row">
+        <div className="col-sm text-center">
+          <div className="card bg-transparent border-0">
+            <Link to="https://github.com/RozierT">
+              <img src={Github} style={{maxHeight:50}}/>
+            </Link>
+          </div>
+        </div>
+        <div className="col-sm text-center">
+          <div className="card bg-transparent border-0">
+            <Link to="https://www.linkedin.com/in/taylor-rozier-a76247105/">
+              <img src={LinkedIn} style={{maxHeight:50}}/>
+            </Link>
+          </div>
+        </div>
+        <div className="col-sm text-center">
+          <div className="card bg-transparent border-0">
+            Email: <a href="mailto:Roziertaylor@gmail.com">roziertaylor@gmail.com</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
+  
+  
 }
 
 export default Footer;
